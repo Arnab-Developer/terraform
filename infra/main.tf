61e28749cc792600bbd1fd4df48c41cb4a143841
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2023-11-09 11:40:17 +0530

    terra main config

---
+++ infra/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.6.3"
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.79.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-org0101-terrastate"
+    storage_account_name = "saorg0101terrastate"
+    container_name       = "terrastate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {}
